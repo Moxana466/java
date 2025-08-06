7d9641328d888c9920b513c60002bb4f965b41fc
Moxana466 <2420030059@gmail.com>
2025-08-06 10:58:57 +0530

    Adding another file of java

---
+++ using_all_datatypes.java
@@ -0,0 +1,16 @@
+package lab3;
+
+public class using_all_datatypes {
+	  public static void main(String[] args) {
+          byte b = 10;
+          short s = 100;
+          int i = 1000;
+          long l = 10000L;
+          float f = 1.2f;
+          double d = 3.14;
+          char c = 'A';
+          boolean flag = true;
+
+          System.out.println(b+" "+s+" "+i+" "+l+" "+f+" "+d+" "+c+" "+flag);
+      }
+}
